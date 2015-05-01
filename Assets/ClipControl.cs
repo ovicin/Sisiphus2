b0748e9819c3c2b056f0b5e738e849b1c1150332
Ovidiu Cincheza <ovicin@yahoo.com>
2015-05-01 13:00:21 +0200

    commit project

---
+++ Assets/ClipControl.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClipControl : MonoBehaviour {
+
+	enum enAccelState{
+		INIT,
+		ACCEL1,
+		ACCEL2
+	};
+	enAccelState state; 
+	float oldxAxisAccel = 0;
+	float currentThreshold = 0;
+	float xAxisThreshold = 30;
+	float yAxisThreshold = 70;
+	float minPlaybackTime = 0.02f;
+	float maxPlaybackTime = 0.98f;
+	float PlaybackTimeDivider = 4;
+	float currentMinPlaybackTime = 0;
+	float currentMaxPlaybackTime = 0;
+	float xAxisAccel = 0;
+	float yAxisAccel = 0;
+	float playbackTime = 0;
+
+
+	bool play;
+	// Use this for initialization
+	void Start () {
+		play = true;
+
+		OSCHandler.Instance.Init();
+		currentMinPlaybackTime = minPlaybackTime;
+		currentMaxPlaybackTime = maxPlaybackTime / PlaybackTimeDivider;
+
+		state = enAccelState.INIT;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/* update the OSC handler */
+		OSCHandler.Instance.UpdateLogs();
+
+		/* update the accell variables */
+		xAxisAccel = OSCHandler.Instance.GetX ();
+		yAxisAccel = OSCHandler.Instance.GetY ();
+
+		/* get current playback time */
+		Animator animator = GetComponent<Animator> ();
+		AnimatorStateInfo currentState = animator.GetCurrentAnimatorStateInfo (0);
+		playbackTime = currentState.normalizedTime % 1;
+
+		//Debug.Log (playbackTime + " " + xAxisAccel);
+
+		Set2AxisPlaybackDividerSpeed ();
+
+
+	}
+
+	private void SetSpeedSimple(){
+		if ((xAxisAccel >= 50) && (playbackTime > 0.02)) {
+			GetComponent<Animator> ().speed = -3;
+		} else if ((xAxisAccel < 50) && (playbackTime < 0.99)) {
+			GetComponent<Animator> ().speed = 3;
+		} else {
+			GetComponent<Animator> ().speed = 0;
+		}
+	}
+
+	private void MouseControledSpeed(){
+
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log ("Mouse Pressed");
+			if (play) {
+				GetComponent<Animator> ().speed = -3;
+				play = false;
+			} else {
+				GetComponent<Animator> ().speed = 3;
+				play = true;
+			}
+		}
+
+	}
+
+	private void Set2AxisPlaybackDividerSpeed(){
+
+		Debug.Log ("Current State " + state);
+		Debug.Log("TimeInterval " + currentMinPlaybackTime + "-" + currentMaxPlaybackTime);
+		//Debug.Log ("CurPlaybakTime " + playbackTime);
+		//Debug.Log ("Accx " + xAxisAccel + " ThresX " + xAxisThreshold);
+		//Debug.Log ("AccY " + yAxisAccel + " ThresY " + yAxisThreshold);
+
+		if (playbackTime < 0 )
+			GetComponent<Animator> ().Play("Take 001", 0, 0.02f);
+	
+		switch (state) {
+		case enAccelState.INIT:
+			currentMinPlaybackTime = minPlaybackTime;
+			currentMaxPlaybackTime = maxPlaybackTime / PlaybackTimeDivider;
+			state = enAccelState.ACCEL2;
+			break;
+		case enAccelState.ACCEL1:
+			if ((xAxisAccel < 50)){
+				state = enAccelState.ACCEL2;
+
+			}
+			else{
+
+				if((playbackTime >= currentMinPlaybackTime) &&
+				   (playbackTime < currentMaxPlaybackTime)){
+					//move backward
+					GetComponent<Animator> ().speed = -3;
+				}
+				else if (playbackTime >= currentMaxPlaybackTime){
+					/* update the playback thresholds */
+					currentMinPlaybackTime = currentMaxPlaybackTime;
+					currentMaxPlaybackTime = maxPlaybackTime/PlaybackTimeDivider;
+					PlaybackTimeDivider--;
+					if (PlaybackTimeDivider == 0){
+						/* start a new game switch to the INIT state */
+						state = enAccelState.INIT;
+					}
+					else{
+						/* switch to the ACCEL2 state */
+						state = enAccelState.ACCEL2;
+					}
+				}
+			}
+			break;
+		case enAccelState.ACCEL2:
+			if (yAxisAccel > 50){
+				state = enAccelState.ACCEL1;
+
+			}
+			else{
+
+				if((playbackTime >= currentMinPlaybackTime) &&
+				   (playbackTime < currentMaxPlaybackTime)){
+					//move forward
+					GetComponent<Animator> ().speed = 3;
+				}
+				else if (playbackTime >= currentMaxPlaybackTime){
+					/* update the playback thresholds */
+					currentMinPlaybackTime = currentMaxPlaybackTime;
+					currentMaxPlaybackTime = maxPlaybackTime/PlaybackTimeDivider;
+					PlaybackTimeDivider--;
+					if (PlaybackTimeDivider == 0){
+						/* start a new game switch to the INIT state */
+						state = enAccelState.INIT;
+					}
+					else{
+						/* switch to the ACCEL2 state */
+						state = enAccelState.ACCEL1;
+					}
+				}
+			}
+			break;
+		}
+
+	}
+
+}
---
+++ Assets/PlayMovieTexture.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityOSC;
+
+public class PlayMovieTexture : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		/*
+		Dictionary<string, ClientLog> clients = new Dictionary<string,ClientLog>();
+		clients = OSCHandler.Instance.Clients;
+		Dictionary<string, ServerLog> servers = new Dictionary<string, ServerLog>();
+		servers = OSCHandler.Instance.Servers;
+		*/
+	}
+}
